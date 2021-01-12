539b19e0abc79a1b78fea0d9c22b997dee832f3e
GJP1106 <gjp1106@outlook.com>
2021-01-13 06:49:38 +0800

    add H264&h265 Encode

--- first_ffmpeg/first_ffmpeg.cpp
+++ first_ffmpeg/first_ffmpeg.cpp
@@ -16,6 +16,116 @@ extern "C"{ //指定函数是c语言函数，函数名不包含重载标注
 #define RGBA_FILE "800_600_25.rgba"
 #define YUV_FILE_1 "400_300_25_1.yuv"
 
+void Encode(int index, char *str)
+{
+	string filename = "400_300_25";
+	AVCodecID codec_id = AV_CODEC_ID_H264;
+	if (index > 1) {
+		string codec = str;
+		if (codec == "h265" || codec == "hevc") {
+			codec_id = AV_CODEC_ID_HEVC;
+		}
+	}
+	if (codec_id == AV_CODEC_ID_H264) {
+		filename += ".h264";
+	}
+	else if (codec_id == AV_CODEC_ID_HEVC) {
+		filename += ".h265";
+	}
+	ofstream ofs;
+	ofs.open(filename, ios::binary);
+	/// 1、找到编码器 AV_CODEC_ID_HEVC(h265)
+	auto codec = avcodec_find_encoder(codec_id);
+	if (!codec) {
+		cerr << "codec not find!" << endl;
+		return;
+	}
+	/// 2、编码上下文
+	auto c = avcodec_alloc_context3(codec);
+	if (!c) {
+		cerr << "avcodec_alloc_context3 failed!" << endl;
+		return;
+	}
+
+	/// 3、设定上下文参数
+	c->width = 400;
+	c->height = 300;
+	// 帧时间戳的时间单位  pts * time_base = 播放时间(秒)
+	c->time_base = { 1, 25 };			// 分数	1 / 25
+
+	c->pix_fmt = AV_PIX_FMT_YUV420P;	// 元数据像素格式，与编码算法相关
+	c->thread_count = 16;				// 编码线程数，可以通过调用系统接口获取cpu核心数量
+
+	/// 4.打开编码器上下文
+	int re = avcodec_open2(c, codec, NULL);
+	if (re != 0) {
+		char buf[1024] = { 0 };
+		av_strerror(re, buf, sizeof(buf) - 1);
+		cerr << "avcodec_open2 faild!" << buf << endl;
+		return;
+	}
+	cout << "avcodec_open2 success!" << endl;
+	/// 创建好AVFrame空间 未压缩数据
+	auto frame = av_frame_alloc();
+	frame->width = c->width;
+	frame->height = c->height;
+	frame->format = c->pix_fmt;
+	re = av_frame_get_buffer(frame, 0);
+	if (re != 0) {
+		char buf[1024] = { 0 };
+		av_strerror(re, buf, sizeof(buf) - 1);
+		cerr << "av_frame_get_buffer faild!" << buf << endl;
+		return;
+	}
+	auto packet = av_packet_alloc();
+	// 十秒视频，250帧
+	for (int i = 0; i < 250; i++) {
+		// 生成AVFrame 数据  每帧数据不同
+		// Y
+		for (int y = 0; y < c->height; y++) {
+			for (int x = 0; x < c->width; x++) {
+				frame->data[0][y * frame->linesize[0] + x] = x + y + i * 3;
+			}
+		}
+		//UV
+		for (int y = 0; y < c->height / 2; y++) {
+			for (int x = 0; x < c->width / 2; x++) {
+				frame->data[1][y * frame->linesize[1] + x] = 128 + y + i * 2;
+				frame->data[2][y * frame->linesize[2] + x] = 64 + x + i * 5;
+			}
+		}
+		frame->pts = i;   // 显示时间
+		// 发送未压缩帧到线程中压缩
+		re = avcodec_send_frame(c, frame);
+		if (re != 0) {
+
+			break;
+		}
+		while (re >= 0) { //返回多帧
+			// 接收压缩帧，一般前几次调用返回空(缓冲，立刻返回，编码未完成)
+            // 编码是在独立的线程中
+			re = avcodec_receive_packet(c, packet);
+			if (re == AVERROR(EAGAIN) || re == AVERROR_EOF) {
+				break;
+			}
+			if (re < 0) {
+				char buf[1024] = { 0 };
+				av_strerror(re, buf, sizeof(buf) - 1);
+				cerr << "avcodec_receive_packet faild!" << buf << endl;
+				break;
+			}
+			cout << packet->size << " " << flush;
+			ofs.write((char*)packet->data, packet->size);
+			av_packet_unref(packet);
+		}
+	}
+	ofs.close();
+	av_packet_free(&packet);
+	av_frame_free(&frame);
+	/// 释放编码器上下文
+	avcodec_free_context(&c);
+}
+
 void YUVToRGBA()
 {
 	int width = 400;
@@ -226,6 +336,7 @@ int main(int argc, char* argv[])
 	// 400x300 YUV 转 RGBA 800x600 并存到文件
 	//YUVToRGBA();
 	cout << "RGBA2YUV" << endl;
-	RGBA2YUV();
+	//RGBA2YUV();
+	Encode(argc, argv[1]);
     return 0;
 }
